ac18fb52627e1cea9ec51a63407e203c15c6688e
newfield-2026 <n.f.org2026@gmail.com>
2026-07-12 23:26:41 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.NFO_CONFIG = {
-  GAS_API_URL: 'https://script.google.com/macros/s/REPLACE_WITH_DEPLOYMENT_ID/exec',
-  GOOGLE_CLIENT_ID: 'REPLACE_WITH_WEB_CLIENT_ID.apps.googleusercontent.com'
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbyI7g8SJPhb2Q_uK8YssCwioE_sHxXRq9EX2rZZ7JS56sIUp2wD6REsu3M1ZMt63TN_/exec',
+  GOOGLE_CLIENT_ID: '653417841411-6vmotr5l910l4spm77kgj8ob5vcsen31.apps.googleusercontent.com'
 };
