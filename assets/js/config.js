375ecb11519ff5f3972d27d50119f5577ee609a1
newfield-2026 <n.f.org2026@gmail.com>
2026-07-12 23:55:29 +0900

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.NFO_CONFIG = {
-  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbyI7g8SJPhb2Q_uK8YssCwioE_sHxXRq9EX2rZZ7JS56sIUp2wD6REsu3M1ZMt63TN_/exec',
+  GAS_API_URL: 'https://nfo-invoice-api.n-f-org2026.workers.dev',
   GOOGLE_CLIENT_ID: '653417841411-6vmotr5l910l4spm77kgj8ob5vcsen31.apps.googleusercontent.com'
 };
